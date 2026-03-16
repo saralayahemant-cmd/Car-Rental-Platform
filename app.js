494200edb8b47a618ca68c71800308ae65af305c
Hemant Saralaya <saralayahemant@gmail.com>
2026-03-16 22:27:44 +0530

    3rd

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const authrouter = require("./routes/authrouter");
 const methodOverride = require("method-override");
 
 const session = require("express-session");
-const MongoStore=require("connect-mongo");
+const MongoStore = require("connect-mongo");
 
 const flash = require("connect-flash");
 const passport = require("passport");
@@ -18,7 +18,7 @@ const User = require("./models/user");
 require("dotenv").config();
 require("./init/cloudinaryconfig");
 
-const dburl=process.env.ATLASDB_URL;
+const dburl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -39,17 +39,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
 
-const store = MongoStore.create({
+const store = new MongoStore({
   mongoUrl: process.env.ATLASDB_URL,
   collectionName: "sessions",
   crypto: {
-    secret: process.env.SECRET
+    secret: process.env.SECRET,
   },
-  touchAfter: 24 * 3600
+  touchAfter: 24 * 3600,
 });
 
-store.on("error",(err)=>{
-  console.log("Error in session store",err);
+store.on("error", (err) => {
+  console.log("Error in session store", err);
 });
 
 const sessionOptions = {
@@ -63,7 +63,6 @@ const sessionOptions = {
     httpOnly: true,
   },
 };
-
 
 app.use(session(sessionOptions));
 app.use(flash());
